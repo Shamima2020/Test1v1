47c32c92aa0e4bcee2534269e7674e0cdeea6376
Shamima2020 <sfhossainrj@gmail.com>
2021-02-03 14:05:59 -0700

    First commit

---
+++ src/GitTest1/Test1.java
@@ -0,0 +1,17 @@
+package GitTest1;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		String x = "I am new in Git";
+		
+		if (x=="I am new in Git")
+			System.out.println("New");
+		else
+		{
+			System.out.println("Old");
+		}
+
+	}
+
+}
